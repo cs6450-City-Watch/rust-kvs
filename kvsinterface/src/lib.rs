a0335bbc715e8a8d789591cc050bf76371683776
ash <ashkernel02@gmail.com>
2025-10-18 14:11:51 -0600

    interface changed a tad to include timestamps on transactions- KvsReplica service for appending entries between replicas

--- kvsinterface/src/lib.rs
+++ kvsinterface/src/lib.rs
@@ -3,6 +3,8 @@ use std::fmt::Display;
 use std::net::SocketAddr;
 use thiserror::Error;
 
+use std::time::SystemTime;
+
 pub type TransactionIdentifier = (SocketAddr, u64);
 
 #[derive(Error, Debug, Serialize, Deserialize)]
@@ -39,9 +41,30 @@ pub type KvsResult<T> = Result<T, KvsError>;
 
 #[tarpc::service]
 pub trait Kvs {
-    async fn begin(tx_no: u64) -> KvsResult<()>;
+    async fn begin(tx_no: u64, ts: SystemTime) -> KvsResult<()>;
+    // IDEA: only the transaction init operation (`begin`) needs to have an associated timestamp
+    // this is to compare all `gets` against
+    // once done, our commit operation will assign a timestamp
+    // to all of the relevant `put`s.
     async fn get(tx_no: u64, key: String) -> KvsResult<u64>;
     async fn put(tx_no: u64, key: String, val: u64) -> KvsResult<()>;
     async fn commit(tx_no: u64) -> KvsResult<()>;
     async fn abort(tx_no: u64) -> KvsResult<()>;
+}
+
+// TimeStampedEntry and KvsReplica could be argued to belong only in kvsserver.
+// Right now, I don't care all that much.
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct TimeStampedEntry {
+    pub ts: SystemTime,
+    pub key: String,
+    pub val: u64,
+}
+
+// not at all fault-tolerant; still applicable since we're more just trying to get consistency across replicas
+// also like the Kvs service, it could be argued that this should be using tonic to not be stuck with rust
+#[tarpc::service]
+pub trait KvsReplica {
+    async fn append_entries(entries: Vec<TimeStampedEntry>) -> KvsResult<()>;
 }
